f034b7f6a632c9affe710fbc1f6edf4c9a0bae42
ziad hwas <ziad.hwas@yomicepa.com>
2023-10-04 14:36:05 +0300

    creating the candiate doctype and it's fields

---
+++ techchallengeapp/candidate/doctype/candidate/candidate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ziad hwas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Candidate', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ techchallengeapp/candidate/doctype/candidate/candidate.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, ziad hwas and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Candidate(Document):
+	pass
